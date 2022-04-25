9c2e7730a611ed886e03c7e5aba78b78fdf12b89
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2022-04-25 12:16:08 +0200

    update to 0.6.0-RC2, use ProcessDocumentation annotation

--- mii-dsf-process-projectathon-data-transfer/src/main/java/de/medizininformatik_initiative/processes/projectathon/data_transfer/spring/config/TransferDataConfig.java
+++ mii-dsf-process-projectathon-data-transfer/src/main/java/de/medizininformatik_initiative/processes/projectathon/data_transfer/spring/config/TransferDataConfig.java
@@ -9,7 +9,7 @@ import org.highmed.dsf.fhir.client.FhirWebserviceClientProvider;
 import org.highmed.dsf.fhir.organization.EndpointProvider;
 import org.highmed.dsf.fhir.organization.OrganizationProvider;
 import org.highmed.dsf.fhir.task.TaskHelper;
-import org.highmed.dsf.tools.generator.Documentation;
+import org.highmed.dsf.tools.generator.ProcessDocumentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -53,76 +53,76 @@ public class TransferDataConfig
 	@Autowired
 	private FhirContext fhirContext;
 
-	@Documentation(required = true, processProperty = true, description = "The base address of the KDS FHIR server to read/store FHIR resources", example = "http://foo.bar/fhir")
+	@ProcessDocumentation(required = true, description = "The base address of the KDS FHIR server to read/store FHIR resources", example = "http://foo.bar/fhir")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.base.url:#{null}}")
 	private String fhirStoreBaseUrl;
 
-	@Documentation(processProperty = true, description = "Client implementation used to connect to the KDS FHIR server in order to read/store FHIR resources")
+	@ProcessDocumentation(description = "Client implementation used to connect to the KDS FHIR server in order to read/store FHIR resources")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.client:de.medizininformatik_initiative.processes.projectathon.data_transfer.client.fhir.KdsFhirClientImpl}")
 	private String fhirStoreClientClass;
 
-	@Documentation(processProperty = true, description = "PEM encoded file with one or more trusted root certificate to validate the KDS FHIR server certificate when connecting via https", recommendation = "Use docker secret file to configure", example = "/run/secrets/hospital_ca.pem")
+	@ProcessDocumentation(description = "PEM encoded file with one or more trusted root certificate to validate the KDS FHIR server certificate when connecting via https", recommendation = "Use docker secret file to configure", example = "/run/secrets/hospital_ca.pem")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.trust.certificates:#{null}}")
 	private String fhirStoreTrustStore;
 
-	@Documentation(processProperty = true, description = "PEM encoded file with client-certificate, if KDS FHIR server requires mutual TLS authentication", recommendation = "Use docker secret file to configure", example = "/run/secrets/kds_server_client_certificate.pem")
+	@ProcessDocumentation(description = "PEM encoded file with client-certificate, if KDS FHIR server requires mutual TLS authentication", recommendation = "Use docker secret file to configure", example = "/run/secrets/kds_server_client_certificate.pem")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.certificate:#{null}}")
 	private String fhirStoreCertificate;
 
-	@Documentation(processProperty = true, description = "Private key corresponding to the KDS FHIR server client-certificate as PEM encoded file. Use *${env_variable}_PASSWORD* or *${env_variable}_PASSWORD_FILE* if private key is encrypted", recommendation = "Use docker secret file to configure", example = "/run/secrets/kds_server_private_key.pem")
+	@ProcessDocumentation(description = "Private key corresponding to the KDS FHIR server client-certificate as PEM encoded file. Use *${env_variable}_PASSWORD* or *${env_variable}_PASSWORD_FILE* if private key is encrypted", recommendation = "Use docker secret file to configure", example = "/run/secrets/kds_server_private_key.pem")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.private.key:#{null}}")
 	private String fhirStorePrivateKey;
 
-	@Documentation(processProperty = true, description = "Password to decrypt the KDS FHIR server client-certificate encrypted private key", recommendation = "Use docker secret file to configure by using *${env_variable}_FILE*", example = "/run/secrets/kds_server_private_key.pem.password")
+	@ProcessDocumentation(description = "Password to decrypt the KDS FHIR server client-certificate encrypted private key", recommendation = "Use docker secret file to configure by using *${env_variable}_FILE*", example = "/run/secrets/kds_server_private_key.pem.password")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.private.key.password:#{null}}")
 	private char[] fhirStorePrivateKeyPassword;
 
-	@Documentation(processProperty = true, description = "Basic authentication username, set if the server containing the FHIR KDS data requests authentication using basic auth")
+	@ProcessDocumentation(description = "Basic authentication username, set if the server containing the FHIR KDS data requests authentication using basic auth")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.basicauth.username:#{null}}")
 	private String fhirStoreUsername;
 
-	@Documentation(processProperty = true, description = "Basic authentication password, set if the server containing the FHIR KDS data requests authentication using basic auth", recommendation = "Use docker secret file to configure by using *${env_variable}_FILE*", example = "/run/secrets/kds_server_basicauth.password")
+	@ProcessDocumentation(description = "Basic authentication password, set if the server containing the FHIR KDS data requests authentication using basic auth", recommendation = "Use docker secret file to configure by using *${env_variable}_FILE*", example = "/run/secrets/kds_server_basicauth.password")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.basicauth.password:#{null}}")
 	private String fhirStorePassword;
 
-	@Documentation(processProperty = true, description = "Bearer token for authentication, set if the server containing the FHIR KDS data requests authentication using a bearer token, cannot be set using docker secrets")
+	@ProcessDocumentation(description = "Bearer token for authentication, set if the server containing the FHIR KDS data requests authentication using a bearer token, cannot be set using docker secrets")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.bearer.token:#{null}}")
 	private String fhirStoreBearerToken;
 
-	@Documentation(processProperty = true, description = "The timeout in milliseconds until a connection is established between the KDS client and the KDS FHIR server", recommendation = "Change default value only if timeout exceptions occur")
+	@ProcessDocumentation(description = "The timeout in milliseconds until a connection is established between the KDS client and the KDS FHIR server", recommendation = "Change default value only if timeout exceptions occur")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.timeout.connect:10000}")
 	private int fhirStoreConnectTimeout;
 
-	@Documentation(processProperty = true, description = "The timeout in milliseconds used when requesting a connection from the connection manager between the KDS client and the KDS FHIR server", recommendation = "Change default value only if timeout exceptions occur")
+	@ProcessDocumentation(description = "The timeout in milliseconds used when requesting a connection from the connection manager between the KDS client and the KDS FHIR server", recommendation = "Change default value only if timeout exceptions occur")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.timeout.connection.request:10000}")
 	private int fhirStoreConnectionRequestTimeout;
 
-	@Documentation(processProperty = true, description = "Maximum period of inactivity in milliseconds between two consecutive data packets of the KDS client and the KDS FHIR server", recommendation = "Change default value only if timeout exceptions occur")
+	@ProcessDocumentation(description = "Maximum period of inactivity in milliseconds between two consecutive data packets of the KDS client and the KDS FHIR server", recommendation = "Change default value only if timeout exceptions occur")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.timeout.socket:10000}")
 	private int fhirStoreSocketTimeout;
 
-	@Documentation(processProperty = true, description = "The KDS client will log additional debug output", recommendation = "Change default value only if exceptions occur")
+	@ProcessDocumentation(description = "The KDS client will log additional debug output", recommendation = "Change default value only if exceptions occur")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.client.verbose:false}")
 	private boolean fhirStoreHapiClientVerbose;
 
-	@Documentation(processProperty = true, description = "Proxy location, set if the server containing the FHIR KDS data can only be reached through a proxy", example = "http://proxy.foo:8080")
+	@ProcessDocumentation(description = "Proxy location, set if the server containing the FHIR KDS data can only be reached through a proxy", example = "http://proxy.foo:8080")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.proxy.url:#{null}}")
 	private String fhirStoreProxyUrl;
 
-	@Documentation(processProperty = true, description = "Proxy username, set if the server containing the FHIR KDS data can only be reached through a proxy which requests authentication")
+	@ProcessDocumentation(description = "Proxy username, set if the server containing the FHIR KDS data can only be reached through a proxy which requests authentication")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.proxy.username:#{null}}")
 	private String fhirStoreProxyUsername;
 
-	@Documentation(processProperty = true, description = "Proxy password, set if the server containing the FHIR KDS data can only be reached through a proxy which requests authentication", recommendation = "Use docker secret file to configure by using *${env_variable}_FILE*")
+	@ProcessDocumentation(description = "Proxy password, set if the server containing the FHIR KDS data can only be reached through a proxy which requests authentication", recommendation = "Use docker secret file to configure by using *${env_variable}_FILE*")
 	@Value("${de.medizininformatik.initiative.kds.fhir.server.proxy.password:#{null}}")
 	private String fhirStoreProxyPassword;
 
-	@Documentation(required = true, processProperty = true, processNames = {
+	@ProcessDocumentation(required = true, processNames = {
 			"medizininformatik-initiativede_dataReceive" }, description = "Location of the COS private-key as 4096 Bit RSA PEM encoded, not encrypted file", recommendation = "Use docker secret file to configure", example = "/run/secrets/cos_private_key.pem")
 	@Value("${de.medizininformatik.initiative.cos.private.key:#{null}}")
 	private String cosPrivateKeyFile;
 
-	@Documentation(required = true, processProperty = true, processNames = {
+	@ProcessDocumentation(required = true, processNames = {
 			"medizininformatik-initiativede_dataReceive" }, description = "Location of the COS public-key as 4096 Bit RSA PEM encoded file", recommendation = "Use docker secret file to configure", example = "/run/secrets/cos_public_key.pem")
 	@Value("${de.medizininformatik.initiative.cos.public.key:#{null}}")
 	private String cosPublicKeyFile;
