06187f78048cdc3a19b164ea64d017e96e697572
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2022-11-14 14:00:04 +0100

    increase all versions to 0.3.1

--- mii-dsf-process-projectathon-data-transfer/src/main/java/de/medizininformatik_initiative/process/projectathon/data_transfer/DataTransferProcessPluginDefinition.java
+++ mii-dsf-process-projectathon-data-transfer/src/main/java/de/medizininformatik_initiative/process/projectathon/data_transfer/DataTransferProcessPluginDefinition.java
@@ -24,7 +24,7 @@ import de.medizininformatik_initiative.processes.kds.client.KdsClientFactory;
 
 public class DataTransferProcessPluginDefinition implements ProcessPluginDefinition
 {
-	public static final String VERSION = "0.3.0";
+	public static final String VERSION = "0.3.1";
 	public static final LocalDate RELEASE_DATE = LocalDate.of(2022, 10, 21);
 
 	@Override
